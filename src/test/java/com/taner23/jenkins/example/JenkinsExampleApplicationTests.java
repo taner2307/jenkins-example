06c42771bf1567e0703b849ed67effac482c94eb
taner2307 <taneracs@hotmail.com>
2024-12-11 23:55:56 +0300

    init

---
+++ src/test/java/com/taner23/jenkins/example/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taner23.jenkins.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
