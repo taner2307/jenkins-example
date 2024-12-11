d0ebf34d584540f332a45e22663337946dc4d912
taner2307 <taneracs@hotmail.com>
2024-12-12 01:39:15 +0300

    init

--- src/main/java/com/taner23/jenkins/example/JenkinsExampleApplication.java
+++ src/main/java/com/taner23/jenkins/example/JenkinsExampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsExampleApplication {
 
 	public static void main(String[] args) {
-		System.out.println("den  eme");
+		System.out.println("deneme");
 		SpringApplication.run(JenkinsExampleApplication.class, args);
 	}
 
